2f74292f8eb0abd52509faefcb6f6167864804ec
tusderechos <hidrosoluciones61@gmail.com>
2025-11-14 09:03:07 -0600

    avances de clase Game y clase base RentItem

--- examenLab1/src/examenlab1/Game.java
+++ examenLab1/src/examenlab1/Game.java
@@ -8,6 +8,92 @@ package examenlab1;
  *
  * @author Hp
  */
-public class Game {
+
+import java.util.Calendar;
+import java.util.ArrayList;
+import javax.swing.JOptionPane;
+
+public class Game extends RentItem implements MenuActions {
     
+    private Calendar FechaPublicacion;
+    private ArrayList<String> Especificaciones;
+    private static final int PRECIO_RENTA = 20;
+    
+    public Game(String CodigoItem, String NombreItem, double PrecioBase) {
+        super(CodigoItem, NombreItem, PrecioBase);
+        
+        FechaPublicacion = Calendar.getInstance();
+        Especificaciones = new ArrayList<>();
+    }
+    
+    @Override
+    public String toString() {
+        return super.toString() + " | Fecha de Publicacion: " + FechaPublicacion + " - PS3 Game";
+    }
+    
+    public void setFechaPublicacion(int year, int mes, int dia) {
+        FechaPublicacion.set(year, mes, dia);
+    }
+    
+    public void listEspecificaciones() {
+        if (Especificaciones.isEmpty()) {
+            JOptionPane.showMessageDialog(null, "Este juego no tiene especificaciones registradas", "Sin especificaciones", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        
+        StringBuilder sb = new StringBuilder();
+        ListEspecificacionesRec(0, sb);
+        
+    }
+    
+    private void ListEspecificacionesRec(int indice, StringBuilder sb) {
+        if (indice == Especificaciones.size()) {
+            return;
+        }
+        
+        sb.append((indice + 1)).append(".").append(Especificaciones.get(indice)).append("\n");
+        ListEspecificacionesRec(indice + 1, sb);
+    }
+    
+    @Override
+    public double PagoRenta(int dias) {
+        double pago = dias * PRECIO_RENTA;
+        
+        return pago;
+    }
+    
+    @Override
+    public void submenu() {
+        int opcion = 0;
+        
+        do {            
+            String eleccion = JOptionPane.showInputDialog(null, """
+                                                Submenu - Game
+                                                1. Actualizar Fecha de Publicacion
+                                                2. Agregar Especificacion
+                                                3. Ver Especificaciones
+                                                4. Salir
+                                                
+                                                Ingrese una opcion de las 4 presente para continuar
+                                                """, "Submenu", JOptionPane.INFORMATION_MESSAGE);
+            
+            if (eleccion == null) {
+                break;
+            }
+            
+            try {
+                opcion = Integer.parseInt(eleccion);
+                
+                ejecutarOption(opcion);
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Ingrese una opcion vaalida (de las 4 presentes)");
+            }
+            
+        } while (opcion != 4);
+    }
+    
+    @Override
+    public void ejecutarOption(int opcion) {
+        
+    }
 }
--- examenLab1/src/examenlab1/RentItem.java
+++ examenLab1/src/examenlab1/RentItem.java
@@ -8,6 +8,37 @@ package examenlab1;
  *
  * @author Hp
  */
-public class RentItem {
+
+public abstract class RentItem {
     
+    protected String CodigoItem;
+    protected String NombreItem;
+    protected double PrecioBase;
+    protected int CantCopias;
+    
+    public RentItem(String CodigoItem, String NombreItem, double PrecioBase) {
+        this.CodigoItem = CodigoItem;
+        this.NombreItem = NombreItem;
+        this.PrecioBase = PrecioBase;
+        CantCopias = 0;
+    }
+    
+    @Override
+    public String toString() {
+        return "Codigo del item: " + CodigoItem + " | Nombre del item: " + NombreItem + " | Precio: " + PrecioBase + " | Cantidad de copias: " + CantCopias;
+    }
+    
+    public abstract double PagoRenta(int dias);
+
+    public String getCodigoItem() {
+        return CodigoItem;
+    }
+
+    public String getNombreItem() {
+        return NombreItem;
+    }
+
+    public double getPrecioBase() {
+        return PrecioBase;
+    }
 }
